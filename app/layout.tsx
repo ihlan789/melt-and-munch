f58c52b0b5baabe6251c34b7a61647d8f9ff9130
ihlan789 <61504292+ihlan789@users.noreply.github.com>
2026-01-27 16:06:20 +0700

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Dimsum website frontend',
+  title: 'Melt & Munch',
   description: 'Generated with BLACKBOX AI Builder',
 }
 
